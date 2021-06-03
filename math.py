df5163138dfa46547bb40ffeec5ba62c90fd192c
hrithikpatni31052000 <61503162+hrithikpatni31052000@users.noreply.github.com>
2021-06-03 14:38:15 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def add(x,y):
+	return x+y
+# added subtract implementation
+def substract(x,y):
+	return x-y
+#added multiply header
+def multiply(x,y):
+	pass
+#added divide header
+def divide(x,y):
+	if y==0:
+		return Divide_by_0_error
+	else:
+		return x/y
